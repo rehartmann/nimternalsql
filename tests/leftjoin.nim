439205efe57aad47bbe6e47966bd3a5dc5a50b78
rehartmann <rhartmann@users.sourceforge.net>
2021-10-24 00:01:44 +0200

    Extended test

--- tests/leftjoin.nim
+++ tests/leftjoin.nim
@@ -9,10 +9,13 @@ exec(db, sql"INSERT INTO emp VALUES(1, 'Bob', 'Base')")
 exec(db, sql"INSERT INTO emp VALUES(2, 'Daisy', 'Database')")
 exec(db, sql"INSERT INTO emp_team VALUES(1, 'Team A')")
 exec(db, sql"INSERT INTO emp_team VALUES(2, 'Team B')")
+exec(db, sql"INSERT INTO emp_team VALUES(3, 'Team C')")
 exec(db, sql"INSERT INTO emp2 VALUES(1, 'Bob', 'Base', 'Team A')")
+exec(db, sql"INSERT INTO emp2 VALUES(4, 'Dusty', 'Database', 'Team C')")
 
 var rows = getAllRows(db, sql"""SELECT * FROM emp_team t
                               LEFT JOIN emp ON t.empno = emp.empno
+                              WHERE t.empno <= 2
                               ORDER BY last_name""")
 doAssert rows.len == 2
 doAssert rows[0][0] == "1"
@@ -29,7 +32,7 @@ doAssert rows[1][4] == "Database"
 rows = getAllRows(db, sql"""SELECT * FROM emp_team LEFT JOIN emp2
                                 ON no = empno
                                 ORDER BY emp_team.team_name""")
-doAssert rows.len == 2
+doAssert rows.len == 3
 doAssert rows[0][0] == "1"
 doAssert rows[0][1] == "Team A"
 doAssert rows[0][2] == "1"
@@ -40,11 +43,16 @@ doAssert rows[1][1] == "Team B"
 doAssert rows[1][2] == ""
 doAssert rows[1][3] == ""
 doAssert rows[1][4] == ""
+doAssert rows[2][0] == "3"
+doAssert rows[2][1] == "Team C"
+doAssert rows[2][2] == ""
+doAssert rows[2][3] == ""
+doAssert rows[2][4] == ""
 
 rows = getAllRows(db, sql"""SELECT * FROM emp_team LEFT JOIN emp2
                                 ON emp_team.team_name = emp2.team_name
                                 ORDER BY emp_team.team_name""")
-doAssert rows.len == 2
+doAssert rows.len == 3
 doAssert rows[0][0] == "1"
 doAssert rows[0][1] == "Team A"
 doAssert rows[0][2] == "1"
@@ -57,3 +65,9 @@ doAssert rows[1][2] == ""
 doAssert rows[1][3] == ""
 doAssert rows[1][4] == ""
 doAssert rows[1][5] == ""
+doAssert rows[2][0] == "3"
+doAssert rows[2][1] == "Team C"
+doAssert rows[2][2] == "4"
+doAssert rows[2][3] == "Dusty"
+doAssert rows[2][4] == "Database"
+doAssert rows[2][5] == "Team C"
